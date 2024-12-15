26766f5ae46bfd1772ce0f4c47d7200fd1d8aae8
Erik Scheffers <eriks5@users.noreply.github.com>
2024-12-15 20:02:52 +0100

    Correct banking timing for 6509

--- rtl/cpu_6509.vhd
+++ rtl/cpu_6509.vhd
@@ -41,6 +41,7 @@ architecture rtl of cpu_6509 is
 	signal localDo : std_logic_vector(7 downto 0);
 	signal localWe : std_logic;
 	signal localSync : std_logic;
+	signal running : std_logic;
 
 	signal localAccess : std_logic;
 	signal exeReg : std_logic_vector(7 downto 0);
@@ -69,6 +70,7 @@ begin
 		Sync    => localSync
 	);
 
+	running     <= '1' when (rdy = '1' or localWe = '0') else '0';
 	localAccess <= '1' when localA(15 downto 1) = X"000"&"000" else '0';
 	localDi     <= localDo when localWe = '0' else std_logic_vector(din) when localAccess = '0' else exeReg when localA(0) = '0' else indReg;
 
@@ -101,20 +103,20 @@ begin
 			elsif enable = '1' then
 				lastA0 <= localA(0);
 				if localSync = '1' then
-					if rdy = '1' and din(7 downto 6) = "10" and din(4 downto 0) = "10001" then
+					if running = '1' and din(7 downto 6) = "10" and din(4 downto 0) = "10001" then
 						indCount <= to_unsigned(1, 3);
 					else
 						indCount <= (others => '0');
 					end if;
-				elsif (indCount = 1 and (rdy = '0' or localA(0) = lastA0)) then
+				elsif (indCount = 1 and (running = '0' or localA(0) = lastA0)) then
 					indCount <= (others => '0');
-				elsif indCount /= 0 then
+				elsif indCount /= 0 and running = '1' then
 					indCount <= indCount + 1;
 				end if;
 			end if;
 		end if;
 	end process;
-
+	
 	addr <= unsigned(localA(15 downto 0));
 	dout <= unsigned(localDo) when localAccess = '0' or widePO = '1' else unsigned("0000" & localDo(3 downto 0));
 	pout <= unsigned(indReg) when localSync = '0' and indCount >= 4 else unsigned(exeReg);
